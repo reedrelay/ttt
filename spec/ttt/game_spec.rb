4a3692905621b479e5d684a998ed3dedbdc4050c
Karl Knutson <karl.knutson@gmail.com>
2012-09-27 15:38:26 -0500

    now with runnable

--- spec/ttt/game_spec.rb
+++ spec/ttt/game_spec.rb
@@ -3,21 +3,16 @@ require 'spec_helper'
 module TicTacToe
   describe Game do
     describe "#start" do
+        let(:output) { double('output').as_null_object }
+        let(:game)   { Game.new(output) }
+
       it "sends a welcome message" do
-        output = double('output').as_null_object
-        game = Game.new(output)
-
         output.should_receive(:puts).with('Welcome to Tic-Tac-Toe.')
-
         game.start
       end
 
       it "prompts for choice of X or O" do
-        output = double('output').as_null_object
-        game = Game.new(output)
-
         output.should_receive(:puts).with('Would you like to play X or O?:')
-
         game.start
       end
     end
