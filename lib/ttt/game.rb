9557bd8c06b3f9a01fe77b87ef2cbfc449f814de
Karl Knutson <karl.knutson@gmail.com>
2012-09-27 15:20:18 -0500

    first green

--- lib/ttt/game.rb
+++ lib/ttt/game.rb
@@ -5,6 +5,8 @@ module TicTacToe
     end
 
     def start
+      @output.puts 'Welcome to Tic-Tac-Toe.'
+      @output.puts 'Would you like to play X or O?:'
     end
   end
 end
--- spec/ttt/game_spec.rb
+++ spec/ttt/game_spec.rb
@@ -4,15 +4,22 @@ module TicTacToe
   describe Game do
     describe "#start" do
       it "sends a welcome message" do
-        output = double('output')
+        output = double('output').as_null_object
         game = Game.new(output)
 
-        output.should_receive(:puts).with('Welcome to Tic-Tac-Toe!')
+        output.should_receive(:puts).with('Welcome to Tic-Tac-Toe.')
 
         game.start
       end
 
-      it "prompts for choice of X or O"
+      it "prompts for choice of X or O" do
+        output = double('output').as_null_object
+        game = Game.new(output)
+
+        output.should_receive(:puts).with('Would you like to play X or O?:')
+
+        game.start
+      end
     end
   end
 end
