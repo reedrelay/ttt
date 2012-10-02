565117ac4463f6da3d19341fdfe0d88b2ded0479
Karl Knutson <karl.knutson@gmail.com>
2012-10-02 08:26:58 -0500

    biting off more fragile nonsense than I can chew

--- lib/ttt/game.rb
+++ lib/ttt/game.rb
@@ -3,7 +3,7 @@ module TicTacToe
     def initialize(output)
       @output = output
       @board = TicTacToe::Board.new(@output)
-      @opponent = TicTacToe::Opponent(@output)
+      @opponent = TicTacToe::Opponent.new(@output)
     end
 
     def start
@@ -11,15 +11,29 @@ module TicTacToe
       @output.puts 'Would you like to play X or O?:'
     end
 
-    def player(player)
+    def set_player(player)
       @player = player
       
       move_usage(@player)
-      @opponent.player()#ternary? player == 'X' ? 'O' : 'X')
+      @opponent.player(other_player(@player))
+    end
+
+    def other_player(player)
+      if player == 'X'
+        return 'O'
+      elsif
+        return 'X'
+      end
+    end
+
+    def player_goes_first
+      if @player == 'X'
+        return true
+      end
     end
 
     def move_usage(player)
-      @output.puts 'Where would you like to play your #{player}?:'
+      @output.puts 'Where would you like to play your #{player} ?:'
       @output.puts 'abc   The board diagram at left shows'
       @output.puts 'def   where your choice will go'
       @output.puts 'ghi   on the grid.'
@@ -32,7 +46,116 @@ module TicTacToe
     def show_board
       @board.show
     end
+
+    def show_end_state
+      @board.show
+    end
+
+    def over?
+
+      if won? == ' '
+        if draw?
+          return true
+        end
+      end
+
+      return false
+
+    end
+
+    def won?
+      
+      for i in 0..2
+        r = @board.row(i)
+        matches, player = score_sequence(r)
+        if matches == 3
+            return player
+        end
+      end
+
+      for i in 0..2
+        c = @board.column(i)
+        matches, player = score_sequence(r)
+        if matches == 3
+            return player
+        end
+      end
+
+      for i in 0..1
+        d = @board.diagonal(i)
+        matches, player = score_sequence(r)
+        if matches == 3
+            return player
+        end
+      end
+
+      return ' '
+
+    end
+
+    def draw?
+      !game_open?
+    end
+
+    def game_open?
+      for i in 0..2
+        r = @board.row(i)
+        if sequence_open?(r) == ' '
+          return true
+        end
+      end
+
+      for i in 0..2
+        c = @board.column(i)
+        if sequence_open?(r) == ' '
+          return true
+        end
+      end
+
+      for i in 0..1
+        d = @board.diagonal(i)
+        if sequence_open?(d) == ' '
+          return true
+        end
+      end
+      
+        return false 
+    end
+
+    def sequence_open?(seq)
+      for i in 0..2
+        if seq[i] == ' '
+          return ' '
+        end
+
+        return seq[0]
+      end
+
+    def score_sequence(seq)
+
+      if seq[0] != ' '
+        if seq[0] == seq[1]
+          if seq[1] == seq[2]
+            return 3, seq[0]
+          elsif
+            return 2, seq[0]
+          end
+        elsif seq[1] != ' '
+          if seq[1] == seq[2]
+            return 2, seq[1]
+          end
+        end
+      elsif seq[1] != ' '
+        if seq[1] == seq[2]
+          return 2, seq[1]
+        end
+      end
+        return 1, seq[0]
+      end
+    end
+
   end
+
   class Board
     def initialize(output)
       
@@ -65,24 +188,93 @@ module TicTacToe
       @output.puts @grid[3..5]
       @output.puts @grid[6..8]
     end
+
+    def row(n)
+      @grid[0+(n*3)..2+(n*3)]
+    end
+
+    def row_move(row, element, player)
+      @grid[element+row*3] = player
+    end
+      
+
+    def column(n)
+      [@grid[n], @grid[n+3], @grid[n+6]]
+    end
+
+    def column_move(column, element, player)
+      @grid[element+column*3] = player
+    end
+
+
+    def diagonal(n)
+      if n == 0 # top left to bottom right
+        [@grid[0], @grid[4], @grid[8]]
+      elsif n == 1
+        [@grid[6], @grid[4], @grid[2]]
+      end
+    end
+
+    def diagonal_move(diagonal, element, player)
+      if diagonal == 0
+          @grid[element*4] = player
+      elsif diagonal == 1
+          @grid[6-element*2] = player
+      end
+    end
+
   end
+
   class Opponent(output)
     def initialize
       @output = output
     end
-
+    
     def player(player)
       @player = player
       
     end
-
+    
     def move(board)
       @board = board
-      # has player won, has opponent won (maybe game checks this post moves.)
-      # for following, ask board for rows, columns, diagonals?
+      
+      # first pass, dumb opponent--looks for first opening?
+      
+      for i in 0..2
+        r = @board.row(i)
+        for j in 0..2
+          if r[j] == ' '
+            @board.row_move(i, j, @player)
+            return true
+          end
+        end
+      end
+
+      for i in 0..2
+        c = @board.column(i)
+        for j in 0..2
+          if c[j] == ' '
+            @board.column_move(i, j, @player)
+            return true
+          end
+        end
+      end
+
+      for i in 0..1
+        d = @board.diagonal(i)
+        for j in 0..2
+          if d[j] == ' '
+            @board.diagonal_move(i, j, @player)
+            return true
+          end
+        end
+      end
+      
       # can I win
       # can I block
       # can I advance
 
-      @board
+      return true
+    end
+  end
 end
